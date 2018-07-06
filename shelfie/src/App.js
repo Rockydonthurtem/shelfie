18b7b13ebef16a8778602d6041ffccce22c55595
Rockydonthurtem <charrisq@hotmail.com>
2018-07-06 11:37:51 -0500

    starting server

--- shelfie/src/App.js
+++ shelfie/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './Component/Dashboard/Dashboard';
+import Header from './Component/Header/Header';
+import Form from './Component/Form/Form';
 
 class App extends Component {
   render() {
@@ -13,6 +16,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Dashboard />
+        <Header />
+        <Form />
       </div>
     );
   }
---
+++ shelfie/src/Component/Product/Product.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class Product extends React() {
+    render() {
+        function prod{
+            return <div>Product</div>
+        }
+    }
+
+}
